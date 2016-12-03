a87bf1686e86b952b1c5bc5932b0201ef0426c41
Marina Limeira <marinaflessa@gmail.com>
2016-12-03 09:52:49 -0300

    Update setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux'
 
 const Pokemon = {
   init: (specie) => ({ level: 1, specie }),
-  setSpecie: (prev, specie) => ({ ...prev, specie }),
-  updateLevel: (prev) => ({ ...prev, level: prev.level + 1}) 
+  setSpecie: (prev, specie) => ({ specie, level: prev.level }),
+  updateLevel: (prev) => ({ specie: prev.specie, level: prev.level + 1}) 
 }
 
